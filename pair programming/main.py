bcea2e1baf22c92e6886bc0057571e262bc76cc3
ShearmanChua <shearman_chua@hotmail.com>
2022-10-12 18:33:32 +0800

    new

---
+++ pair programming/main.py
@@ -0,0 +1,28 @@
+from sys import stdin
+import re
+
+def check_valid_string(input_text):
+    valid_str = re.compile(r'.*\(\).*', re.IGNORECASE)
+    if (valid_str.match(input_text) and "() ->" not in input_text) or "\" + \"" in input_text or (not input_text.split(",")[-1].strip().isalnum() and "() ->" not in input_text):
+        return True
+    else:
+        return False
+
+lines = stdin.readlines()
+
+complete_statements = []
+complete_statement_line = []
+ran_lines = []
+for line in range(0,len(lines)):
+    complete_line = ""
+    if not lines[line].startswith('//') and "logger.debug" in lines[line]:
+        complete_line += lines[line]
+        count = 1
+        while ";" not in complete_line:
+            complete_line += lines[line + count]
+            count += 1
+        complete_line = complete_line.replace("\n","")
+        complete_line = complete_line[complete_line.find("logger.debug(")+13:complete_line.find(";") - 1]
+        
+        if check_valid_string(complete_line):
+           print(line + 1)
